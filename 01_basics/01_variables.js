efdc2f56873f0767c5a0d27d8be07cba64477b31
Souvik2003Maity <113382121+Souvik2003Maity@users.noreply.github.com>
2024-07-12 19:01:08 +0000

    variable basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144534;
+let accountEmail="souvik2003maity@gmail.com";
+var accountPassword=12345;
+accountCity="Jaipur";
+
+// accountId=2 not allowed as it is const
+
+accountEmail="souvik2002maity@gmail.com";
+accountPassword=54321;
+accountCity="Mumbai";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
