9ec237edd6e22a842fd3065370dac1f99aec2f45
Souvik2003Maity <souvik2003maity@gmail.com>
2024-07-13 23:14:40 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3);  this gives us error as we are using node js here not browser
+
+// premitive datatypes
+
+// number
+// BigInt
+// String=>""
+// Boolean=>true/false;
+// null=> standalone value
+// undefined
+// Symbol=>unique
+
+
+// Object
+
+console.log(typeof undefined);
+console.log(typeof null);
